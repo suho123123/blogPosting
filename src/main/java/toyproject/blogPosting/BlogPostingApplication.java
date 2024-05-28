c5f8cf8777f617e515f106e4687b464b265ad131
suho <yousuho1232@naver.com>
2024-05-28 17:12:42 +0900

    [commit] first commit

---
+++ src/main/java/toyproject/blogPosting/BlogPostingApplication.java
@@ -0,0 +1,13 @@
+package toyproject.blogPosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPostingApplication.class, args);
+	}
+
+}
