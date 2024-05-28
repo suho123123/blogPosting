6cfe0e5d788fad9fb1761c0c74e97a69065c8c62
suho <yousuho1232@naver.com>
2024-05-28 17:05:40 +0900

    [commit] first commit

---
+++ test/java/toyproject/blogPosting/BlogPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.blogPosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
